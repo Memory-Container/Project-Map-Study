520cd8f54e5c95d807f7e113fe966236289d00f4
Night Furry <ddhieu2707@gmail.com>
2026-02-01 18:52:58 +0700

    added modal

--- pages/roadmap/roadmap.js
+++ pages/roadmap/roadmap.js
@@ -1,22 +1,60 @@
-// src = "https://code.jquery.com/jquery-3.7.1.min.js";
-// integrity = "sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=";
-// crossorigin = "anonymous";
-const overlay = document.getElementById("overlay");
-const lock = document.getElementById("lock");
-const learn = document.getElementById("learn");
-const lockChangeBackground = document.getElementById("lockChangeBackground");
-const demo = document.getElementById("demo");
-
-function openOverlay() {
-    overlay.style.display = "flex";
+function startCourse() {
+    let start = document.querySelector('#start-course')
+    start.style.display = 'none'
+    let firstChapter = document.querySelector('.course-chapter')
+    firstChapter.classList.remove('disabled')
+    let firstChapterOptions = document.querySelector(".course-chapter .chapter-options")
+    firstChapterOptions.classList.remove('hidden')
+    closeModal()
 }
-function closeOverlay() {
-    overlay.style.display = "none";
+function startCourseConfirmation() {
+    openModal({
+        title: "Bạn có muốn theo lộ trình này?",
+        message: "Nếu đồng ý bạn sẽ không thể chọn một lộ trình khác cho đến khi hoàn thành lộ trình này hoặc tử bỏ học lộ trình này",
+        options: [
+            {
+                type: 'tertiary',
+                message: 'Hủy bỏ', 
+                callback: closeModal
+            },
+            {
+                type: 'primary',
+                message: 'Chấp nhận', 
+                callback: startCourse
+            }
+        ]
+    })
 }
-function chooseOverlay() {
-    learn.style.display = "flex";
-    overlay.style.display = "none";
-    lock.style.display = "none";
-    lockChangeBackground.style.display = "flex";
-    demo.style.display = "none";
+function startChapter() {
+    let statusContainer = document.querySelector(".chapter-status.hidden")
+    let startChapterButton = document.querySelector("#start-chapter")
+    let skipChapterButton = document.querySelector(".tertiary.hidden.skip")
+    let lock = document.querySelector('.lock')
+    lock.classList.add('active')
+    lock.textContent = "1"
+    skipChapterButton.classList.remove("hidden")
+    startChapterButton.classList.add("hidden")
+    statusContainer.classList.remove('hidden')
+    closeModal()
+}
+function startChapterConfirmation() {
+    openModal({
+        title: "Bạn muốn bắt đầu học chương này?",
+        message: `
+            Bạn sẽ phải làm hết bài tập và đọc hết nội dung của chương này để có thể chuyển qua chương tiếp theo<br>
+            <span class="small-info">Bạn vẫn có thể bỏ qua chương này trong quá trình học nếu có nhu cầu</span>
+        `,
+        options: [
+            {
+                type: 'tertiary',
+                message: 'Hủy bỏ', 
+                callback: closeModal
+            },
+            {
+                type: 'primary',
+                message: 'Chấp nhận', 
+                callback: startChapter
+            }
+        ]
+    })
 }
